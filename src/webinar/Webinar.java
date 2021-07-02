81257472fc3aeefdbac831f3bbaa6ec10cb7701b
Aulia Rahmadhani <aulia20ti@mahasiswa.pcr.ac.id>
2021-07-01 23:41:34 -0700

    Revert "back to previous"
    This reverts commit 4302f953c896744aafe611da12bb72537679bbf6.

--- src/webinar/Webinar.java
+++ src/webinar/Webinar.java
@@ -16,6 +16,7 @@ public class Webinar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello! I,m studying about git");
     }
     
 }
