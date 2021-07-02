44c00810f9f7f90c04b0e9cf9831a2790904660e
Aulia Rahmadhani <aulia20ti@mahasiswa.pcr.ac.id>
2021-07-01 23:45:48 -0700

    perubahan pertama di branch1

--- src/webinar/Webinar.java
+++ src/webinar/Webinar.java
@@ -17,6 +17,7 @@ public class Webinar {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello! I,m studying about git");
+        System.out.println("Hello Again! I'm in branch one ");
     }
     
 }
